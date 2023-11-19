372b7b41b2134a5f6093add4644deabe598c0f33
Chebupela <aristarhsakulin@gmail.com>
2023-11-19 15:31:34 +0300

    routes fixed

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -5,6 +5,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AuthPage from "../pages/AuthPage";
 import RegisterPage from "../pages/RegisterPage";
+import MoneyReceivePage from "../pages/MoneyReceivePage";
+import MoneySendPage from "../pages/MoneySendPage";
+import CommitTransactionPage from "../pages/CommitTransactionPage";
+import SuccessTransactionPage from "../pages/SuccessTransactionPage";
 
 
 export function RoutesComponent() {
@@ -15,6 +19,11 @@ export function RoutesComponent() {
 				<Route path='/register' element={ <RegisterPage /> } />
 
 				<Route path='/homepage' element={ <HomePage /> } />
+				<Route path='/moneyReceivePage' element={ <MoneyReceivePage /> } />
+				<Route path='/moneySend' element={ <MoneySendPage /> } />
+
+				<Route path='/commitTransaction' element={ <CommitTransactionPage /> } />
+				<Route path='/successTransaction' element={ <SuccessTransactionPage /> } />
 			</Routes>
 		</Router>
 	)
